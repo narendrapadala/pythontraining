c08cae4735d5fc5dc02f732ce6e10420c1579a9e
Kalyan311 <84614385+Kalyan311@users.noreply.github.com>
2021-05-27 18:19:03 +0530

    Delete read.py

--- read.py
+++
@@ -1,9 +0,0 @@
-with open('readwritepython.txt') as f:
-    lines=f.readlines()
-    for l in lines:
-        print(l)
-        f.close()
-        
-#f = open("readwritepython.txt", "r")
-#print(f.read())
-#f.close()
