0bc9d531618163889e23ed1129d2b7858229698d
Kalyan311 <84614385+Kalyan311@users.noreply.github.com>
2021-05-28 13:01:33 +0530

    Update celsius.py

--- Kalyan/variables,operators,expressions/celsius.py
+++ Kalyan/variables,operators,expressions/celsius.py
@@ -1,3 +1,3 @@
-celsius=37
+celsius=int(input("enter a temprature value in celsius:-"))
 fahrenheit=(celsius*1.8)+32
-print("%.2f celsius=%.2f fahrenheit"%(celsius,fahrenheit))
+print("%.2f celsius = %.2f fahrenheit"%(celsius,fahrenheit))
