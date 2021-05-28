4f059968be37d737539756fba2ad107f8f84e1a1
Kalyan311 <84614385+Kalyan311@users.noreply.github.com>
2021-05-28 16:05:58 +0530

    Update simple and compound interest.py

--- Kalyan/variables,operators,expressions/simple and compound interest.py
+++ Kalyan/variables,operators,expressions/simple and compound interest.py
@@ -2,9 +2,9 @@ p=float(input("enter principal amount"))
 r=float (input("enter annual rate oof interest"))
 t=float(input("enter time in number of years"))
 
-si=p*r*t/100
+#si=p*r*t/100
 
 ci=p*(1+r)**t
 
-print("simple interest is",si)
+#print("simple interest is",si)
 print("compound interest is",ci)
