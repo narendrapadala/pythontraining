ea0cb739aba2bc06a40df41117fad0f94d7ae47d
Kalyan311 <84614385+Kalyan311@users.noreply.github.com>
2021-05-28 14:44:43 +0530

    Update Area of circle.py

--- Kalyan/variables,operators,expressions/Area of circle.py
+++ Kalyan/variables,operators,expressions/Area of circle.py
@@ -2,3 +2,5 @@ pi=3.14
 r=float(input("enter the radius of the circle:"))
 area=pi*r*r
 print("area of the circle is",area)
+circumference=2*pi*r
+print("circumference of the circle is ",circumference)
